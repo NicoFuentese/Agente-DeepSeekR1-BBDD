72e0653942118dd7bb8ade751a83319658392db8
nicofuentese <nfuentee@chilquintas.cl>
2026-03-05 12:27:03 +0000

    prompt engineering del agente

---
+++ queries/presentacion_asistente.sql
@@ -0,0 +1,7 @@
+/* Hola! Soy un asistente SQL especializado en MariaDB. 
+Para ayudarte con consultas específicas, necesitaría:
+1. Descripción de tablas disponibles
+2. Relaciones entre ellas
+3. Tu requerimiento analítico particular
+
+¿Qué datos necesitas explorar hoy? */
